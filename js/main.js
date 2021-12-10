a722083e4fcd3807447db1e67d0fc5b5cee44418
Nathan Ahn <nahn1@hawk.iit.edu>
2021-12-09 19:08:40 -0600

    Show current date js at the bottom of home page

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function showDate() {
+    var d = new Date();
+    var curr_date = d.getDate();
+    var curr_month = d.getMonth() + 1; //months are zero based
+    var curr_year = d.getFullYear();
+    document.write( curr_month + "-" + curr_date + "-" + curr_year);
+  }
+  showDate();
